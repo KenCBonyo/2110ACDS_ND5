9d9367392d6fe4a9b310d7d6e2ad3d4405c2c508
teddywaweru <teddywaweru@gmail.com>
2022-02-17 10:52:59 +0300

    220217 - API change, new default notebook

--- api.py
+++ api.py
@@ -56,10 +56,15 @@ def model_prediction():
     # response with our API.
     return jsonify(output)
 
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
 # Configure Server Startup properties.
 # Note:
 # When developing your API, set `debug=True`
 # This will allow Flask to automatically restart itself everytime you
 # update your API code.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='127.0.0.1', port=5000, debug=False)
